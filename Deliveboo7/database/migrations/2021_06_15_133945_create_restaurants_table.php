ae051719f90acd9477da553c86d14293917336c2
Gabriele Alessi <89archaon89@gmail.com>
2021-06-15 16:08:54 +0200

    creazione migrations

---
+++ Deliveboo7/database/migrations/2021_06_15_133945_create_restaurants_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('email', 64) -> nullable(false);
+            $table -> string('password', 64) -> nullable(false);
+            $table -> string('nome_attivita', 64) -> nullable(false);
+            $table -> string('via', 64) -> nullable(false);
+            $table -> string('n_civico', 8) -> nullable(false);
+            $table -> string('citta', 64) -> nullable(false);
+            $table -> string('cap', 8) -> nullable(false);
+            $table -> string('p_iva', 16) -> nullable(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
