6f826ccc84e8440bbe19c6915884aca135d86642
Gabriele Alessi <89archaon89@gmail.com>
2021-06-17 13:55:28 +0200

    restaurant with real data

--- Deliveboo7/database/factories/RestaurantFactory.php
+++ Deliveboo7/database/factories/RestaurantFactory.php
@@ -6,15 +6,115 @@ use App\Restaurant;
 use Faker\Generator as Faker;
 
 $factory->define(Restaurant::class, function (Faker $faker) {
+
+
+    $restaurants = [
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+
+        ],
+        [
+          'email' => 'gabbo1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+        ],
+        [
+          'email' => 'mirko1@gmail.com',
+          'password' =>'ciao',
+          'nome_attivita' =>'Mattarallo',
+          'via' =>'Via del grano',
+          'n_civico' =>'300',
+          'citta' =>'Roma',
+          'cap' =>'00014',
+          'p_iva'=>'0126641452',
+
+        ]
+    ];
+
+    $index = $faker -> unique()-> numberBetween(0, 8);
+    $restaurant = $restaurants[$index];
+
     return [
 
-        'email' => $faker -> email,
-        'password' => $faker -> password,
-        'nome_attivita' => $faker -> sentence($nbWords = 2, $variableNbWords = true),
-        'via' => $faker -> streetName,
-        'n_civico' => $faker -> buildingNumber,
-        'citta' => $faker -> city,
-        'cap' => $faker -> postcode,
-        'p_iva' => $faker -> ean13,
+        'email' => $restaurant['email'],
+        'password' => $restaurant['password'],
+        'nome_attivita' => $restaurant['nome_attivita'],
+        'via' => $restaurant['via'],
+        'n_civico' => $restaurant['n_civico'],
+        'citta' => $restaurant['citta'],
+        'cap' => $restaurant['cap'],
+        'p_iva' => $restaurant['p_iva'],
     ];
 });
