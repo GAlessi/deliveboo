67cf4b339028237aff35b8a05961354e7db8012e
Mirko <mirkosbaglia2001@gmail.com>
2021-06-18 17:10:46 +0200

    Login/Logout/Register

--- Deliveboo7/routes/web.php
+++ Deliveboo7/routes/web.php
@@ -13,14 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-//MAIN
-Route::get('main', 'MainController@main')
+Route::get('/', 'MainController@main')
        ->name('main');
+
+// //MAIN
+// Route::get('main', 'MainController@main')
+//        ->name('main');
