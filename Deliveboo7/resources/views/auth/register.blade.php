cc7cb097b5d77a3f3d1e840c4fbba34a16a32c23
valerio sgura <valeriosgura88@gmail.com>
2021-06-22 16:07:05 +0200

    update register, createDish

--- Deliveboo7/resources/views/auth/register.blade.php
+++ Deliveboo7/resources/views/auth/register.blade.php
@@ -1,32 +1,27 @@
 @extends('layouts.main-layout')
 
 @section('content')
-  @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <div id="register">
         <div class="mycontainer">
 
             <h2>Registrati</h2>
             <div class="register_container">
-                {{-- <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div> --}}
-
-                {{-- <div class="card-body"> --}}
+               
                 <form class="flex_col align_cen" method="POST" action="{{ route('postRegistration') }}">
                     @csrf
 
-                    {{-- <div class="form-group row"> --}}
+                    {{-- nome --}}
                     <label for="name">Nome</label>
-
-                    {{-- <div class="col-md-6"> --}}
                     <input id="name" type="text" name="name" value="{{ old('name') }}" required autocomplete="name"
                         autofocus>
 
@@ -35,13 +30,9 @@
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
-                    {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
+          
+                    {{-- email --}}
                     <label for="email">Email</label>
-
-                    {{-- <div class="col-md-6"> --}}
                     <input id="email" type="email" name=" email" value="{{ old('email') }}" required autocomplete="email">
 
                     @error('email')
@@ -49,13 +40,9 @@
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
-                    {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                    <label for="password" >Password</label>
-
-                    {{-- <div class="col-md-6"> --}}
+             
+                    {{-- password --}}
+                    <label for="password">Password</label>
                     <input id="password" type="password" name="password" required autocomplete="new-password">
 
                     @error('password')
@@ -63,109 +50,50 @@
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
-                    {{-- </div> --}}
-                    {{-- </div> --}}
+                
+                    {{-- conferma password --}}
+                    <label for="password-confirm">Conferma Password</label>
+                    <input id="password-confirm" type="password" name="password_confirmation" required
+                        autocomplete="new-password">
+                 
+                    {{-- nome attività --}}
+                    <label for="nome_attivita">Nome Attività</label>
+                    <input id="nome_attivita" type="text" name="nome_attivita" required>
+                    
+                    {{-- via --}}
+                    <label for="via">Via</label>
+                    <input id="via" type="text" name="via" required>
+                    
+                    {{-- civico --}}
+                    <label for="n_civico">Numero Civico</label>
+                    <input id="n_civico" type="integer" name="n_civico" required>
+             
+                    {{-- città --}}
+                    <label for="citta">Città</label>
+                    <input id="citta" type="text" name="citta" required>
+                  
+                    {{-- cap --}}
+                    <label for="cap">C.A.P.</label>
+                    <input id="cap" type="text" name="cap" required>
 
-                    {{-- <div class="form-group row"> --}}
-                        <label for="password-confirm">Conferma Password</label>
+                    {{-- p iva --}}
+                    <label for="p_iva">Numero Partita IVA</label>
+                    <input id="p_iva" type="text" name="p_iva" required>
+                   
+                    {{-- checkbox tipologia --}}
+                    <label>
+                        @foreach ($types as $type)
+                            <input id="types_id[]" name="types_id[]" type="checkbox" class="form-check-input"
+                                value="{{ $type->id }}"> {{ $type->nome }}
+                        @endforeach
+                    </label>
 
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="password-confirm" type="password" name="password_confirmation"
-                                required autocomplete="new-password">
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="nome_attivita"
-                            >Nome Attività</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="nome_attivita" type="text" name="nome_attivita" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="via">Via</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="via" type="text" name="via" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="n_civico">Numero Civico</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="n_civico" type="integer" name="n_civico" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="citta">Città</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="citta" type="text" name="citta" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="cap">C.A.P.</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="cap" type="text" name="cap" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- <div class="form-group row"> --}}
-                        <label for="p_iva">Numero Partita IVA</label>
-
-                        {{-- <div class="col-md-6"> --}}
-                            <input id="p_iva" type="text" name="p_iva" required>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
-
-                    {{-- Checkbox --}}
-                    {{-- <div class="form-group row">
-                                          <div class="form-check">
-                                            <label class="form-check-label">
-                                              <input type="checkbox" class="form-check-input" value="">Option 1
-                                            </label>
-                                          </div>
-                                          <div class="form-check">
-                                            <label class="form-check-label">
-                                              <input type="checkbox" class="form-check-input" value="">Option 2
-                                            </label>
-                                          </div>
-                                          <div class="form-check">
-                                            <label class="form-check-label">
-                                              <input type="checkbox" class="form-check-input" value="" disabled>Option 3
-                                          </label>
-                                      </div>
-                                  </div> --}}
-
-
-                    {{-- <div> --}}
-                        <select id="types_id[]" name="types_id[]" required multiple>
-                            @foreach ($types as $type)
-                                <option value="{{ $type->id }}">
-                                    {{ $type->nome }}
-                                </option>
-                            @endforeach
-                        </select>
-                    {{-- </div> --}}
-
-
-                    {{-- <div class="form-group row mb-0"> --}}
-                        {{-- <div class="col-md-6 offset-md-4"> --}}
-                            <button type="submit">
-                                Registrati
-                            </button>
-                        {{-- </div> --}}
-                    {{-- </div> --}}
+                    <button type="submit">
+                        Registrati
+                    </button>
+                    
                 </form>
             </div>
         </div>
-        {{-- </div>
-                    </div>
-                </div> --}}
+        
     @endsection
--- Deliveboo7/resources/views/pages/createDish.blade.php
+++ Deliveboo7/resources/views/pages/createDish.blade.php
@@ -15,28 +15,34 @@
                     @csrf
                     @method('POST')
 
+                    {{-- nome --}}
                     <label for="nome">Nome</label>
                     <input id="nome" type="text" name="nome" value="{{ old('nome') }}"
                         placeholder="Inserisci qui il nome del nuovo elemento" required autofocus>
 
+                    {{-- descrizione --}}
                     <label for="descrizione">Descrizione</label>
                     <input id="descrizione" type="text" name="descrizione" value="{{ old('descrizione') }}"
                         placeholder="Inserisci la descrizione" required>
 
+                    {{-- ingredienti --}}
                     <label for="ingredienti">Ingredienti</label>
                     <input id="ingredienti" type="text" name="ingredienti" value="{{ old('ingredienti') }}"
                         placeholder="Inserisci qui gli ingredienti" required>
 
+                    {{-- prezzo --}}
                     <label for="">Prezzo</label>
                     <input id="prezzo" type="integer" name="prezzo" value="{{ old('prezzo') }}"
                         placeholder="Inserisci qui il prezzo" required>
 
+                    {{-- visibilità --}}
                     <label for="visibilita">Desideri che sia subito visibile nel tuo Menu?</label>
                     <select id="visibilita" name="visibilita" required>
                         <option value=1 selected>Si</option>
                         <option value=0>No</option>
                     </select>
 
+                    {{-- submit --}}
                     <button type="submit">
                         Salva il nuovo elemento
                     </button>
