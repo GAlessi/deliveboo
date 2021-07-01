885a44a33234d4d226cdcb88df0026f8a9124e82
valerio sgura <valeriosgura88@gmail.com>
2021-07-01 11:46:57 +0200

    stats ordini e piatti

--- Deliveboo7/resources/views/pages/statistiche.blade.php
+++ Deliveboo7/resources/views/pages/statistiche.blade.php
@@ -10,7 +10,9 @@
             <div class="mycontainer">
                 <h2>Le statistiche dei tuoi ordini</h2>
                 <div class="graph_container">
+
                     <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+
                 </div>
             </div>
         </div>
@@ -25,41 +27,54 @@
         console.log(myUsers);
         var xValues = ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'];
         var yValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        var yValuesDishes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        var barColors = ["red", "green", "blue", "orange", "brown", "magenta", "yellow", "purple", "pink", "cyan", "brown",
-            "red"
-        ];
+        //var barColors = ["red", "green", "blue", "orange", "brown", "magenta", "yellow", "purple", "pink", "cyan", "brown",
+        //     "red"
+        // ];
 
         //estrazione del mese da created_at
         for (var i = 0; i < myUsers.length; i++) {
 
-            
             let month = new Date(myUsers[i].created_at);
             let indexMonth = month.getMonth();
 
             for (var j = 0; j < yValues.length; j++) {
 
                 //paragono il valore numerico del mese con l'indice dell'array yValues
-
-                if (indexMonth == j && !idOrders.includes(myUsers[i].order_id) ) {
+                if (indexMonth == j && !idOrders.includes(myUsers[i].order_id)) {
                     idOrders.push(myUsers[i].order_id)
                     yValues[j]++
                 };
             };
+
+            for (let x = 0; x < yValuesDishes.length; x++) {
+                if (indexMonth == x) {
+                    yValuesDishes[x]++;
+                } 
+            }
         };
 
         new Chart("myChart", {
             type: "bar",
             data: {
                 labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
+                datasets: [
+                    {
+                        label: 'Ordini',
+                        backgroundColor: '#2a9d8f',
+                        data: yValues
+                    },
+                    {
+                        label: 'Piatti',
+                        backgroundColor: '#f4a261',
+                        data: yValuesDishes
+                    },
+                ],
             },
             options: {
                 legend: {
-                    display: false
+                    display: true
                 },
                 title: {
                     display: true,
@@ -67,6 +82,5 @@
                 }
             }
         });
-
     </script>
 @endsection
